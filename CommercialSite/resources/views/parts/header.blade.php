85fb3e393ac9900406892c8488e88636a5b4657f
Páger Dániel <67972651+TripleSec99@users.noreply.github.com>
2023-04-07 17:27:57 +0200

    Create project, homepage

---
+++ CommercialSite/resources/views/parts/header.blade.php
@@ -0,0 +1,22 @@
+<div class="row">
+    <div class="col text-center d-flex">
+        <div class="col-md-6 text-start">
+            <h1 class="font-semibold dark:text-gray-400">Rodeo Ride</h1>
+        </div>
+        <div class="col-md-6">
+            @if (Route::has('login'))
+                <div class="text-right">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
