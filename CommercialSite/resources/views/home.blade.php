ae801b7df475102e6596bde63eaebf22f72f42c3
Páger Dániel <67972651+TripleSec99@users.noreply.github.com>
2023-04-08 17:33:15 +0200

    favicon

--- CommercialSite/resources/views/home.blade.php
+++ CommercialSite/resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Cruise Now</title>
+        <link rel="icon" type="image/x-icon" href="img/cruiser.jpg">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
