20d501acf7c49dfa9c256344499a7f0e30d637f2
Páger Dániel <67972651+TripleSec99@users.noreply.github.com>
2023-04-15 14:44:23 +0200

    Corrections

--- CommercialSite/resources/views/layouts/guest.blade.php
+++ CommercialSite/resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                     <img src="img/cruiser.jpg" alt="">
                 </a>
             </div>
-            
+
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
